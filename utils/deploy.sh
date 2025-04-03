282e89035d60c4dc7b389476a3617474bce9984f
setteZ <furgorsin@gmail.com>
2025-04-04 00:03:36 +0200

    mqtt library as submodule

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 ampy --port /dev/ttyUSB0 put ./env-node/main.py
 ampy --port /dev/ttyUSB0 put ./env-node/sht30/sht30.py
-ampy --port /dev/ttyUSB0 put ./env-node/mqtt.py
+ampy --port /dev/ttyUSB0 put ./env-node/micropython-mqtt/src/mqtt.py
