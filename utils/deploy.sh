2d3d4e35a6484fd1babd0bad79610bea5bba0513
setteZ <furgorsin@gmail.com>
2025-05-18 11:43:04 +0200

    wrong path for verison file

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -3,5 +3,5 @@ ampy --port /dev/ttyUSB0 put ./env-node/boot.py
 ampy --port /dev/ttyUSB0 put ./env-node/main.py
 ampy --port /dev/ttyUSB0 put ./env-node/sht30/sht30.py
 ampy --port /dev/ttyUSB0 put ./env-node/micropython-mqtt/src/mqtt.py
-ampy --port /dev/ttyUSB0 put ./env-node/micropython-mqtt/src/version
+ampy --port /dev/ttyUSB0 put ./env-node/version
 ampy --port /dev/ttyUSB0 put ./env-node/micropython_ota/micropython_ota.py
