868b07492db38dbb808e9a115cc3226040f8aaa8
setteZ <furgorsin@gmail.com>
2025-06-01 00:32:18 +0200

    new bash to deploy the release on the ta server and make capability

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-
+SHELL=/bin/bash
 VERSION := $(shell cat ./env-node/version | tr -d '\n')
 
 put:
@@ -11,6 +11,19 @@ run-broker:
 	@bash -c "mosquitto -c ./broker/mosquitto.conf"
 
 .PHONY: release
-release:
+release: clean
 	mkdir -p release-node/$(VERSION)
-	@bash -c "python ./utils/make-manifest.py -b ./utils/put.sh -n -o release-node/$(VERSION)"
+	python ./utils/make-release.py -b ./utils/put.sh -n -o release-node/$(VERSION)
+
+deploy-ota: release
+	@if [ -f .env ]; then \
+		echo "Sourcing .env file..."; \
+		set -a; \
+		. .env; \
+		set +a; \
+	fi; \
+	./utils/deploy_node_update.sh $(VERSION)
+	@echo "done"
+
+clean:
+	@rm -r release-node
---
+++ utils/deploy_node_update.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+if [ -z "$1" ]; then
+    echo "Error: Missing argument." >&2
+    exit 1
+fi
+
+if [ -z "$SSH_USER" ]; then
+    echo "Error: Missing \$SSH_USER." >&2
+    exit 1
+fi
+
+if [ -z "$SERVER_IP_NODE_OTA" ]; then
+    echo "Error: Missing \$SERVER_IP_NODE_OTA." >&2
+    exit 1
+fi
+
+if [ -z "$NODE_RELEASES_PATH" ]; then
+    echo "Error: Missing \$NODE_RELEASES_PATH." >&2
+    exit 1
+fi
+
+if [ ! -d "release-node/$1" ]; then
+    echo "$1 does not exist in folder release-node"
+    exit 1
+fi
+
+if [ -n "$SSH_PRIVATE_KEY" ]; then
+    ID_FILE="-i $SSH_PRIVATE_KEY"
+else
+    ID_FILE=""
+fi
+
+scp $ID_FILE -r release-node/* $SSH_USER@$SERVER_IP_NODE_OTA:$NODE_RELEASES_PATH
+ssh $ID_FILE $SSH_USER@$SERVER_IP_NODE_OTA "cd $NODE_RELEASES_PATH && echo $1 > version"
